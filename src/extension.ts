55a25b34290974e75fa961fde2e6c496d3da7b0f
gebeto <slavik.nychkalo@gmail.com>
2021-06-01 13:35:57 +0300

    Added initialization action. Added script importing action

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as child_process from 'child_process';
 import * as path from 'path';
 import * as glob from 'glob';
-import { allowedNodeEnvironmentFlags } from 'process';
+
 
 const USER: string = process.env["USER"]!;
 const SCRIPTABLE_PATH = `/Users/${USER}/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents`
@@ -19,19 +19,39 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "scriptable" is now active!');
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('scriptable.initialize', () => {
+			const command = `ln -s "${SCRIPTABLE_PATH}" sources`;
+			const cwd = getCurrentWorkspacePath().toString();
+			if (cwd) {
+				child_process.exec(command, { cwd: cwd }, () => {
+					vscode.window.showInformationMessage(`Sources are initialized!`, 'OK');
+					vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+				});
+			}
+		})
+	);
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('scriptable.importScript', () => {
-		glob.glob(`${SCRIPTABLE_PATH}/*.js`, {}, (er, files) => {
-			const fileNames = files.map(file => path.basename(file, ".js"))
-			vscode.window.showQuickPick(fileNames).then(res => {
-				vscode.window.showInformationMessage(`Selected: ${res}`);
-			})
-		});
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('scriptable.importScript', () => {
+			glob.glob(`${SCRIPTABLE_PATH}/*.js`, {}, (er, files) => {
+				const fileNames = files.map(file => path.basename(file, ".js"))
+				vscode.window.showQuickPick(fileNames).then(selected => {
+					const command = `ln "${SCRIPTABLE_PATH}/${selected}.js" "${selected}.js"`;
+					const cwd = getCurrentWorkspacePath().toString();
+					if (cwd) {
+						child_process.exec(command, { cwd: cwd }, function() {
+							vscode.window.showInformationMessage(`${selected} Imported!`, 'OK');
+							vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer');
+						});
+					}
+				})
+			});
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
